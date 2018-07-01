d89ddcd67362cb73afeb66863aceb40b58017d63
Swaroop Devaraju <sdevaraju1@student.gsu.edu>
2018-07-01 13:05:50 -0400

    3rd assignment

---
+++ Assignment3/part3.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<body>
+	<?php
+	$month = array ("January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"); 
+	$length = count($month);
+	for($i = 0; $i < $length ; $i++)
+	{
+		echo ($month[$i]);
+		echo"<br>";
+	}
+	echo "<br>";
+	echo "Sorted months:";
+	echo "<br>";
+	echo "<br>";
+	sort($month);
+	for($i = 0; $i < $length; $i++)
+	{
+		echo $month[$i];
+		echo "<br>";
+	}
+	$month = array ("January", "February", "March", "April",
+		"May", "June", "July", "August",
+		"September", "October", "November", "December"); 
+	echo"<br>";
+	echo " Display of  months using foreach:";
+	echo"<br>";
+	echo"<br>";
+	foreach($month as $value)
+	{
+		echo ($value);
+		echo"<br>";
+	}
+	echo"<br>";
+	echo " Display of sorted  months using foreach:";
+	echo"<br>";
+	echo"<br>";
+	sort($month);
+	foreach($month as $value)
+	{
+		echo ($value);
+		echo"<br>";
+	}
+	?>
+</body>
+</html>
